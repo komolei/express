d66ecbaca9d0baa2f29b779b89b957a7b2b90c3e
komolei <944545149@qq.com>
2017-09-11 02:15:06 +0800

    add monitor img Load situation

--- app/view/js/index.js
+++ app/view/js/index.js
@@ -1,3 +1,12 @@
+//load aniamte
+let loadAniamte = () => {
+    let loadAniamte = document.getElementsByClassName('spinner')[0];
+    loadAniamte.classList.remove('hide');
+}
+let missAniamte = () => {
+    let loadAniamte = document.getElementsByClassName('spinner')[0];
+    loadAniamte.classList.add('hide');
+}
 //signIn
 let signInDialog = document.getElementById('signInDialog');
 signInDialog.addEventListener('click', () => {
@@ -7,6 +16,8 @@ let signInBtn = document.getElementById('signInBtn');
 console.log("signInBtn:", signInBtn);
 let isLockUp = false;
 signInBtn.addEventListener('click', () => {
+    loadAniamte();
+    let loadAniamte = document.getElementsByClassName('spinner')[0];
     let email = document.getElementById('inputEmail').value;
     let username = document.getElementById('inputName').value;
     let password = document.getElementById('inputPassword').value;
@@ -19,12 +30,15 @@ signInBtn.addEventListener('click', () => {
     if (isLockUp) return;
     isLockUp = true;
     xhr.onreadystatechange = (username, password) => {
+        // the data loaded,the loadAniamte miss
+        missAniamte();
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             console.log("gg", xhr.responseText);
             let text = xhr.responseText;
             document.getElementById('signInDialog').innerHTML = "<a href='javascript:void(0)'>welcome " + JSON.parse(text).username + "</a>"
             // hide Dialog
             document.getElementsByClassName('signInDialog')[0].classList.remove('changeDialog');
+
             alert('sign In')
             return isLockUp = false;
         }
@@ -62,6 +76,7 @@ signUpBtn.addEventListener('click', () => {
     /\S{6,12}/.test(password) ? three = true : console.log("password error");
     document.getElementById('inputPassword1').value = '';
     document.getElementById('inputSurePassword1').value = '';
+    loadAniamte();
     if (one && two && three && password === surePassword) {
         let xhr = new XMLHttpRequest(),
             method = 'POST',
@@ -69,6 +84,7 @@ signUpBtn.addEventListener('click', () => {
         if (isLockUp) return;
         isLockUp = true;
         xhr.onreadystatechange = (username, password) => {
+            missAniamte();
             if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
                 console.log("gg", xhr.responseText);
                 let text = xhr.responseText;
@@ -93,15 +109,178 @@ signUpBtn.addEventListener('click', () => {
     }
 })
 
+//img load 
+
+// let img = () => {
+//     let imgLoad = document.getElementById('imgLoad');
+//     let imgBox = document.getElementById('imgBox');
+//     console.log(imgLoad.complete, imgBox);
+//     let imgLoadSrc = imgLoad.getAttribute("data-src");
+//     let img = new Image();
+//     // let img=document.createElement("img");
+//     img.src = imgLoadSrc;
+//     // img.setAttribute('src', imgLoadSrc); //all right
+//     // console.log(img.complete, img);
+
+//     let load = () => {
+//         console.log("complete", img.complete, img);
+//         // let a=new Date();
+//         if (img.complete) {
+//             // imgBox.innerHTML = '<img src="http://s.amazeui.org/media/i/demos/bing-1.jpg">';
+//             // imgBox.innerHTML=img; //error
+//             // imgBox.appendChild(img);
+//             imgBox.replaceChild(img, imgLoad);
+//             clearInterval(clearTime);
+//         }
+//     }
+//     let clearTime = setInterval(load, 1000);
+//     // if (img.complete) {
+//     //     imgBox.replaceChild(img, imgLoad);
+//     // }
+
+//     console.log("img:", img);
+//     // img.onload = function (img) {
+//     //     console.log("addImg",img);
+//     //     let imgBox = document.getElementById('imgBox');
+//     //     imgBox.innerHTML = img
+//     // }
 
 
+// }
+
+// img();
+
+//wrap img function
+
+// let wrapImg = (toWrapImg, wrapElement) => {
+//     let imgLoadSrc = toWrapImg.getAttribute("data-src");
+//     let img = new Image();
+//     img.src = imgLoadSrc;
+//     let load = () => {
+//         if (img.complete) {
+//             wrapElement.replaceChild(img, toWrapImg);
+//             clearInterval(clearTime);
+
+//         }
+//         // img.onload=()=>{
+//         //       wrapElement.replaceElement(img,toWrapImg);
+//         // }
+//     }
+//     let clearTime = setInterval(load, 1000);
+
+// }
+// wrapImg(
+//     document.getElementById('imgLoad'),
+//     document.getElementById('imgBox')
+// );
+
+// carousel imgLoad function 
+
+// let carousel = document.querySelectorAll(".carousel-inner .item") //css style in Writing
+// let carouselImg = document.querySelectorAll('.carousel-inner .item>img')
+// Array.prototype.slice.call(carouselImg).map((element, index) => {
+//     // console.log("element:", element, carousel);
+//     wrapImg(element, carousel[index]);
+// })
+
+// array img load 
+
+let arrayImgLoad = (wrappedElements, wrappedElementArea) => {
+    let wrapImg = (WrappedImg, wrappedElement) => {
+        let imgLoadSrc = WrappedImg.getAttribute("data-src");
+        let img = new Image();
+        img.src = imgLoadSrc;
+        let load = () => {
+            if (img.complete) {
+                wrappedElement.replaceChild(img, WrappedImg);
+                clearInterval(clearTime);
+            }
+            // img.onload=()=>{
+            //       wrapElement.replaceElement(img,toWrapImg);
+            // }
+        }
+        let clearTime = setInterval(load, 1000);
+    }
+    Array.prototype.slice.call(wrappedElements).map((element, index) => {
+        // console.log("element:", element, carousel);
+        wrapImg(element, wrappedElementArea[index]);
+    })
+}
+
+arrayImgLoad(
+    document.querySelectorAll(".carousel-inner .item>img"),
+    document.querySelectorAll('.carousel-inner .item')
+)
+arrayImgLoad(
+    document.querySelectorAll("#imgBox>img"),
+    document.querySelectorAll('#imgBox')
+)
 
 
+// monitor img loading situation //监视img加载情况
+let imgLoad = () => {
+    // let img_arr = ['/images/1.jpg', '/images/2.jpg', '/images/3.jpg'];
+    let imgArray = document.querySelectorAll("img")
+    let imgSrcArray = [];
+    Array.prototype.slice.call(imgArray).map((Element, index) => {
+        // let imgSrcArray=[];
+        imgSrcArray.push(Element.getAttribute('data-src'));
+        //duplication array //数组去重
+        // click for more information 
+        //https://www.toobug.net/article/array_unique_in_javascript.html
+        // imgSrcArray = new Set(imgSrcArray)
+        // console.log("set ",set);
+        return imgSrcArray;
+    })
+    // console.log("imgArray:", imgArray, "imgSrcArray", imgSrcArray); 
+    // need change set type to array type ,use Array.from();
+    imgSrcArray = Array.from(new Set(imgSrcArray));
+    console.log("imgArray:", imgArray, "imgSrcArray", imgSrcArray);
 
+    if (imgSrcArray.length != 0) {
+        let img_arr = imgSrcArray;
+        // console.log("img_arr:", img_arr);
+        let nums = img_arr.length;
+        let start = 0;
+        for (let i in img_arr) {
+            let img = document.createElement('img');
+            img.src = img_arr[i];
+            (function (j) {
+                img.onload = function () {
+                    start++;
+                    if (start == nums) {
+                        console.log('全部加载完成');
+                    }
+                    // document.getElementById('loading').style.width = (start / nums) * 100 + '%';
+                    // combine bootstrap
+                    let targetNode = document.getElementById('progressBar');
+                    // let longShow = (start / nums) * 100 +'%';
+                    let longShow = Math.floor((start / nums) * 100)+ '%';
+                    console.log("longShow", longShow);
+                    targetNode.style.width = longShow;
+                    targetNode.innerText = longShow;
+                };
+                img.onerror = function () {
+                    start;
+                    console.log(start,j, img_arr[j] + '失败');
+                    // document.getElementById('loading').style.width = (start / nums) * 100 + '%';
+                    // combine bootstrap
+                    // document.getElementById('progressBar').style.width = (start / nums) * 100 + '%';
+                    let targetNode = document.getElementById('progressBar');
+                    // let longShow = (start / nums) * 100 + '%';
+                    // longShow = Math.floor(longShow)
+                    let longShow = Math.floor((start / nums) * 100)+ '%';
+                    targetNode.style.width = longShow;
+                    targetNode.innerText = longShow;
+                }
+            })(i);
+        }
 
+    }
 
+}
 
-
+imgLoad();
 
 
 
